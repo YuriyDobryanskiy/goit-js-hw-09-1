a62afc16a448f4a4bd6391e68db262e0ff663de4
Yuriy Dobryanskiy <dobryansky@i.ua>
2023-12-30 16:55:08 +0200

    first4

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,5 @@
 //import SimpleLightbox from 'simplelightbox';
-//import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
   {
